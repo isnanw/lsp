164d010def8130d720fae1f75779acfdec93e55d
isnan <isnan@gin.co.id>
2020-09-23 22:22:15 +0900

    istirahat dolo

--- application/views/admin/sertifikasi/skemaSertifikasi.php
+++ application/views/admin/sertifikasi/skemaSertifikasi.php
@@ -83,12 +83,69 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                   <td><image height="50px" src="<?= base_url('assets/images/sertifikasi/').$data['image'] ?>"></image></td>
                   <td> <?=$data['nama'];?></td>
                   <td> <?= substr($data['deskripsi'],0,25).'...';?></td>
-                  <td><button>Edit</button><button>Hapus</button></td>
+                  <td>
+                      <a href="#" class="btn btn-success btn-sm" data-toggle="modal" data-target="#updateDataLokasi<?= $data['id'] ?>"><i class="fa fa-edit "></i></a>
+                      <a href="" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteDataLokasi<?= $data['id'] ?>"><i class="fa fa-trash-o"></i></a>
+                  </td>
                 </tr>
-                
-                <?php
-                    }
-                ?>
+                <!--update data-->
+                  <div class="modal fade" id="updateDataLokasi<?= $data['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="addNewDataLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="addNewDataLabel">Update Data </h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <form action="<?= base_url('admin/updateDataLokasi') ?>" method="post">
+                          <div class="modal-body">
+                            <input type="hidden" name="kode" id="kode" value="<?= $data['id'] ?>">
+                            <!-- <label for="">Lokasi</label>
+                            <div class="form-group">
+                              <input class="form-control" type="text" name="lokasi" id="lokasi" value="<?= $data['lokasi'] ?>">
+                            </div>
+                            <label for="">Titik X</label>
+                            <div class="form-group">
+                              <input class="form-control" type="text" name="x" id="x" value="<?= $data['x'] ?>">
+                            </div>
+                            <label for="">Titik Y</label>
+                            <div class="form-group">
+                              <input class="form-control" type="text" name="y" id="y" value="<?= $data['y'] ?>">
+                            </div> -->
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                          </div>
+
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                  <!--delete data-->
+                  <div class="modal fade" id="deleteDataLokasi<?= $data['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="addNewDataLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="addNewDataLabel">Hapus Data</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <p>Anda yakin ingin menghapus data lokasi '<?= $data['nama'] ?>'</p>
+                        </div>
+
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <a href="<?= base_url('admin/deleteDataLokasi?kode=') ?><?= $data['id'] ?>" class="btn btn-primary">Hapus</a>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+                    <?php } ?>
                 </tbody>
                 <!-- <tfoot>
                 <tr>
